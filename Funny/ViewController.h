75e89d55836aa9ef48b7d716ea14b61c184e1571
Keep Yu <yuconora@163.com>
2015-09-17 09:11:46 +0800

    Initial Commit

---
+++ Funny/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Funny
+//
+//  Created by qianfeng on 15/9/17.
+//  Copyright (c) 2015年 __None__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Funny/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Funny
+//
+//  Created by qianfeng on 15/9/17.
+//  Copyright (c) 2015年 __None__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
